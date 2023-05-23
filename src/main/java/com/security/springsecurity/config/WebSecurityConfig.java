15407f4ce3f058befaf3e3abac5eb34088a4e83f
[FadyGamilM] <1700961@eng.asu.edu.eg>
2023-05-23 20:04:05 +0200

    FEATURE | implement the configs to override the default spring security configs

---
+++ src/main/java/com/security/springsecurity/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.security.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return null;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
